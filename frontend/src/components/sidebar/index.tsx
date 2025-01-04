6ac2d661e4ddc13ecf6557e1df688c45a6c13b49
gabrielmedsilva <gabrielmedsilva@outlook.com>
2025-01-04 10:21:25 -0300

    feat: Sidebar desktop

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -2,7 +2,14 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { Sheet, SheetTrigger, SheetContent, SheetTitle } from "../ui/sheet";
 import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@radix-ui/react-tooltip";
+import {
   Home,
+  LogOut,
   Package,
   PanelBottom,
   Settings2,
@@ -13,6 +20,131 @@ import {
 export default function Sidebar() {
   return (
     <div className="flex w-full flex-col bg-muted-40">
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col">
+        <nav className="flex flex-col items-center gap-4 px-2 py-5">
+          <TooltipProvider>
+            <Link
+              href="#"
+              className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full"
+            >
+              <Package className="w-4 h-4" />
+              <span className="sr-only">Dashboard Avatar</span>
+            </Link>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Home className="w-5 h-5" />
+                  <span className="sr-only">Início</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Início
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <ShoppingBag className="w-5 h-5" />
+                  <span className="sr-only">Pedidos</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Pedidos
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Package className="w-5 h-5" />
+                  <span className="sr-only">Produtos</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Produtos
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Users className="w-5 h-5" />
+                  <span className="sr-only">Clientes</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Clientes
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Settings2 className="w-5 h-5" />
+                  <span className="sr-only">Configurações</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Configurações
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </nav>
+        <nav className="mt-auto flex flex-col items-center gap-4 py-5">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <LogOut className="w-5 h-5 text-red-500" />
+                  <span className="sr-only">Sair</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Sair
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </nav>
+      </aside>
+
       <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
           <Sheet>
@@ -74,6 +206,7 @@ export default function Sidebar() {
                   <SheetTitle>Configurações</SheetTitle>
                 </Link>
               </nav>
+              <nav className="mt-auto flex flex-col items-center gap-4 py-5"></nav>
             </SheetContent>
           </Sheet>
           <h2>Menu</h2>
