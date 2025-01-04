c30e127e1f6d827a2246dd96e77fa2b427992aa9
gabrielmedsilva <gabrielmedsilva@outlook.com>
2025-01-04 08:55:22 -0300

    feat: Estrutura do sidebar

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -1,3 +1,29 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Sheet, SheetTrigger, SheetContent } from "../ui/sheet";
+
 export default function Sidebar() {
-  return <h1>Sidebar</h1>;
+  return (
+    <div className="flex w-full flex-col bg-muted-40">
+      <div className="flex flex-col">
+        <header>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button>
+                <span>Abrir</span>
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent>
+              <nav>
+                <Link href="#">
+                  <span>Logo</span>
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </header>
+      </div>
+    </div>
+  );
 }
