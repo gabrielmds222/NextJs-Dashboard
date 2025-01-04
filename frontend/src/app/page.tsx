5bcfc77cbdd2d7bc3bbc1d4e94f76241faaa7389
gabrielmedsilva <gabrielmedsilva@outlook.com>
2025-01-04 08:43:36 -0300

    feat: Inicio do projeto
    - Adiciona Shadcn UI ao projeto

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl">Projeto Dashboard</h1>
+    </div>
+  );
+}
